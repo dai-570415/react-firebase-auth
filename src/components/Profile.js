87825907bb5e02e0d7fef344d213956eca356aec
Dai <let__it__be@hb.tp1.jp>
2020-05-12 01:39:11 +0900

    変更しました

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignOut from '../elements/SignOut';
+import firebase from '../Firebase';
 
 const Profile = () => {
+    const user = firebase.auth().currentUser;
+    let email, uid;
+
+    if (user != null) {
+        email = user.email;
+        uid = user.uid;
+    }
+
     return (
         <React.Fragment>
             <h2>Profile</h2>
             <Link to="/">Home</Link>
+                <p>ID: { uid }</p>
+                <p>Email: { email }</p>
             <SignOut />
         </React.Fragment>
     );
